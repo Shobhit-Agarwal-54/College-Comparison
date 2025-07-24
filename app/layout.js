a8a4acf484b39ee92f9b980f01e241dc487b6ca9
Shobhit Agarwal <shobhitagarwal2003@gmail.com>
2025-07-25 00:47:02 +0530

    Adding microsoft clarity

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Script from "next/script";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -32,6 +32,19 @@ export default function RootLayout({ children }) {
     <html lang="en"
     >
       <head>
+
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/YOUR_PROJECT_ID";
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "sk1c54p2p3");
+            `,
+          }}
+          />
       <meta name="google-site-verification" content="Pkh1pGVVCt7c6Uo6rF9BRw-qVdDuKTyWa8d3zFXkHlE" />
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
@@ -41,9 +54,18 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased
         `}
-      >
+        >
         {children}
       </body>
     </html>
   );
 }
+
+{/* <script type="text/javascript">
+    (
+      function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "sk1c54p2p3");
+</script> */}
