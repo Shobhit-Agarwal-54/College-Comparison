c784dc6d792a608720ae356e6670ac93f93bd976
Shobhit Agarwal <shobhitagarwal2003@gmail.com>
2025-07-27 18:00:49 +0530

    Fixed errors

--- app/layout.js
+++ app/layout.js
@@ -38,11 +38,11 @@ export default function RootLayout({ children }) {
           id="clarity-script" 
           dangerouslySetInnerHTML={{
             __html: `
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/YOUR_PROJECT_ID";
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "sk1c54p2p3");
+       (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "sk1c54p2p3");
             `,
           }}
           />
